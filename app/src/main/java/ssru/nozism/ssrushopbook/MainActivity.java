889e00dd31fde8e67027bfb4a0ac8c22c21f0dd9
nozism <hud_kung@hotmail.com>
2016-05-30 10:56:49 +0700

    Start App

---
+++ app/src/main/java/ssru/nozism/ssrushopbook/MainActivity.java
@@ -0,0 +1,13 @@
+package ssru.nozism.ssrushopbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
