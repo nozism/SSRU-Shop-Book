582a5178c4a3909e5d15892cdd1ec2e88243a881
nozism <hud_kung@hotmail.com>
2016-05-31 11:48:00 +0700

    SQLite finish

---
+++ app/src/main/java/ssru/nozism/ssrushopbook/MyManage.java
@@ -0,0 +1,18 @@
+package ssru.nozism.ssrushopbook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Pc on 31/5/2559.
+ */
+public class MyManage {
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+    public MyManage(Context context) {
+
+        myOpenHelper = new MyOpenHelper(context);
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+
+    }//Constructor
+}//Main class
